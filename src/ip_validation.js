6889479fce377a8c31628b2f70565fc7f556336f
VlagimirPokhodnya <pokhodnya-ngs.ru@yandex.ru>
2023-12-17 04:40:08 +0700

    add ip_validation

--- src/ip_validation.js
+++ src/ip_validation.js
@@ -25,15 +25,3 @@ function isValidIP(str) {
 }
 
 
-
-console.log(isValidIP("0.0.0.0") + " " + "1");
-console.log(isValidIP("12.255.56.1") + " " + "2");
-console.log(isValidIP("137.255.156.100") + " " + "3");
-console.log(isValidIP('') + " " + "4");
-console.log(isValidIP('abc.def.ghi.jkl') + " " + "5");
-console.log(isValidIP('123.456.789.0') + " " + "6");
-console.log(isValidIP('12.34.56') + " " + "7");
-console.log(isValidIP('01.02.03.04') + " " + "8");
-console.log(isValidIP('256.1.2.3') + " " + "9");
-console.log(isValidIP('1.2.3.4.5') + " " + "10");
-console.log(isValidIP('123,45,67,89') + " " + "11");
