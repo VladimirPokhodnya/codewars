478f72bac1840c185bb1f855c6298f6458993f6f
VlagimirPokhodnya <pokhodnya-ngs.ru@yandex.ru>
2023-12-19 02:10:44 +0700

    Magic Index

---
+++ src/20231210-20231218/20231219-20231231/Magic_Index.js
@@ -0,0 +1,11 @@
+// return the magic index
+function findMagic(arr) {
+  let magic = -1;
+  for (let i = 0; i < arr.length; i++) {
+    if (i == arr[i]) {
+      magic = i;
+      break;
+    }
+  }
+  return magic;
+}
