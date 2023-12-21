e72faae73dcb7f35832f90635fbc98ce4beafa76
VlagimirPokhodnya <pokhodnya-ngs.ru@yandex.ru>
2023-12-22 04:01:40 +0700

    Write Number in Expanded Form

---
+++ src/20231219-20231231/Write_Number_in_Expanded_Form.js
@@ -0,0 +1,15 @@
+function expandedForm(num) {
+  let res = [];
+  let tmp;
+  let mul = 1
+  while (num >= 1) {
+    tmp = (num - Math.floor(num / 10) * 10) * mul;
+    mul *= 10;
+    res.push(tmp);
+    console.log(tmp);
+    num = Math.floor(num / 10);
+  }
+  return res.reverse().join(' + ');
+}
+
+console.log(expandedForm(70304));
