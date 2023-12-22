3594501546d9e87a639d8821677f5c5aa7fc3f83
VlagimirPokhodnya <pokhodnya-ngs.ru@yandex.ru>
2023-12-23 01:06:48 +0700

    write_number 2

---
+++ src/20231219-20231231/Write_Number_Part_2.js
@@ -0,0 +1,20 @@
+function expandedForm(num) {
+  let res = [];
+  let tmp;
+  let mul = 1
+  let tmp2 = num.toString().split('.')[1];
+  while (num >= 1) {
+    tmp = (num - Math.floor(num / 10) * 10) * mul;
+    mul *= 10;
+    if (tmp != 0) {
+      res.push(Math.floor(tmp));
+    }
+    num = Math.floor(num / 10);
+  }
+  console.log(tmp2);
+  res = res.reverse().join(' + ');
+  return res;
+}
+
+// console.log(expandedForm(70304));
+console.log(expandedForm(70304.23056));
