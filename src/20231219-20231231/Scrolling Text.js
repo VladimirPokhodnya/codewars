d959d115a580f13c8d0f914734badf3d5fbcd79e
VlagimirPokhodnya <pokhodnya-ngs.ru@yandex.ru>
2023-12-24 02:01:55 +0700

    Scrolling Text

--- src/20231219-20231231/Scrolling Text.js
+++ src/20231219-20231231/Scrolling Text.js
@@ -1,6 +1,5 @@
 function scrollingText(text) {
   let result = Array(text.length).fill(text.toUpperCase()).map((str, i, arr) => str = i != 0 ? str[str.length - 1] + str.slice(1, str.length - 1) + str[0] : str);
-  // str[str.length-1] + str.slice(1, str.length-1) + str[0]
   return result;
 }
 
