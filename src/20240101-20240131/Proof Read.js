e1066165edf5d6f714e4bd903d6b9ba5074b2448
VlagimirPokhodnya <pokhodnya-ngs.ru@yandex.ru>
2024-01-01 02:30:32 +0700

    Proof Read

---
+++ src/20240101-20240131/Proof Read.js
@@ -0,0 +1,11 @@
+function proofread(str) {
+  return str.split('. ')
+    .filter((a) => a != '')
+    .map((a) => a.toLowerCase().trim().replace(/ie/g, 'ei'))
+    .map((a, i) => a = a.substring(0, 1).toUpperCase() + a.substring(1))
+    .join('. ')
+}
+
+
+console.log(proofread("ShE deCIeved HiM."));
+console.log(proofread("ThiEr DEcIEt wAs INconcIEVablE. sHe SIeZeD thE moMENT."));
