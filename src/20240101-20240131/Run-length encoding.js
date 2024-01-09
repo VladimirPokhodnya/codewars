a21effc5aa5af2bb6c8701533f33690c173069e4
VlagimirPokhodnya <pokhodnya-ngs.ru@yandex.ru>
2024-01-09 09:50:29 +0700

    Run-length encoding.js

--- src/20240101-20240131/Run-length encoding.js
+++ src/20240101-20240131/Run-length encoding.js
@@ -1,17 +1,27 @@
 var runLengthEncoding = function (str) {
 
-  tmp = str.split('')
-  tmp = [...new Set(tmp)].map(a => a.split(""))
+  res = str
+    .split('')
+    .map(a => a.split(""))
+    .flatMap(a => [[1, a[0]]])
 
-  for (let a of tmp) {
-    a.unshift(str.split(a).length - 1)
+  let count = 0
+  for (let i = 1; i < res.length; i++) {
+    if (res[i][1] === res[i - 1][1]) {
+      res[i][0] += ++count
+      res[i - 1].push('del')
+    } else {
+      count = 0
+    }
   }
 
-  return tmp
+  return res.filter(a => a.length != 3);
 }
 
 
 console.log(runLengthEncoding(""));
 console.log(runLengthEncoding("abc"));
 console.log(runLengthEncoding("aab"));
-console.log(runLengthEncoding("hello world"));
+console.log(runLengthEncoding("hello world!"));
+console.log(runLengthEncoding("aaaaabbbb"));
+
